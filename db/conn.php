fd021a8e5956a91a89c8ebb0702a3186098d53e1
Revonda Mcleod <revondamcleod@gmail.com>
2020-11-10 14:16:23 -0500

    Connect Remote Database

--- db/conn.php
+++ db/conn.php
@@ -1,13 +1,20 @@
 <?php 
     
-    $host='127.0.0.1';
-    $db='attendance_db';
-    $user='root';
-    $pass='';
-    $charset='utf8mb4';
-    $dsn="mysql:host=$host;dbname=$db;charset=$charset";
+    // $host='127.0.0.1';
+    // $db='attendance_db';
+    // $user='root';
+    // $pass='';
+    // $charset='utf8mb4';
+   
 
-    
+    //Remote Database
+    $host = 'remotemysql.com';
+    $db = 's0BPB3xTNb';
+    $user = 's0BPB3xTNb';
+    $pass = 'lu20tqTOKH';
+    $charset = 'utf8mb4';
+
+    $dsn="mysql:host=$host;dbname=$db;charset=$charset";
     try {
         $pdo= new PDO ($dsn, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
